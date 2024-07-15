de15d27fe98781e0e44124ef0cb4c588bc817121
christus2024 <christus.tchassi.pro@gmail.com>
2024-07-15 16:04:43 +0200

    ajout du microservice myasso-association-service

---
+++ myasso-services/myasso-association-service/src/main/java/fr/it4innov/myasso/association/MyassoAssociationServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.it4innov.myasso.association;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyassoAssociationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyassoAssociationServiceApplication.class, args);
+    }
+
+}
---
+++ myasso-services/myasso-association-service/src/test/java/fr/it4innov/myasso/association/MyassoAssociationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.it4innov.myasso.association;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyassoAssociationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
