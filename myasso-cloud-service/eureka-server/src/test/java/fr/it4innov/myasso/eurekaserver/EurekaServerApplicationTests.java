e3b3f49b48c5749fb2a7104cc1abdd4985b4957a
christus2024 <christus.tchassi.pro@gmail.com>
2024-07-23 22:48:11 +0200

    Ajout du serveur spring-eureka-server pour la decouverte de service

---
+++ myasso-cloud-service/eureka-server/src/test/java/fr/it4innov/myasso/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.it4innov.myasso.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
